f3baf645e9d8b147903a0ac08b948fb28d092290
alexvision <alex.manusu@gmail.com>
2015-09-12 11:43:40 -0700

    Added chatterbox client

---
+++ pure-sql-server/controllers/index.js
@@ -0,0 +1,28 @@
+var models = require('../models');
+
+module.exports = {
+  messages: {
+    get: function (req, res) {
+      models.messages.get(req, res);
+
+
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      models.messages.post(req.body);
+      res.status(201).send();
+    } // a function which handles posting a message to the database
+  },
+
+  users: {
+    // Ditto as above
+    get: function (req, res) {
+      models.users.get(req, res);    
+    },
+    post: function (req, res) {
+      models.users.post(req.body);
+      res.status(201).send();
+       
+    }
+  }
+};
+
